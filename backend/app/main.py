5ec6fec8bab50a9228a0611e68cfc5df2020307c
epi4mack <rudenokmax1488@gmail.com>
2025-05-12 02:28:15 +0300

    #3 Реализован эндпоинт для получений сообщений пользователя

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,19 +7,28 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
 class MessageCreate(BaseModel):
     name: str
     text: str
+
 
 @app.get("/")
 def read_root():
     return {"message": "SnakeChat v1.0"}
 
+
 @app.get("/messages/")
 def get_messages(db: Session = Depends(get_db)):
     return db.query(Message).all()
 
-@app.post("/messages/") 
+
+@app.get("/messages/{username}/")
+def get_messages_by_user(username: str, db: Session = Depends(get_db)):
+    return db.query(Message).filter(Message.name == username).all()
+
+
+@app.post("/messages/")
 def post_message(msg: MessageCreate, db: Session = Depends(get_db)):
     message = Message(name=msg.name, text=msg.text)
     db.add(message)
